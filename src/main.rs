e9486887b3177716f85c84643782133627ee0dab
Veyyr <dimapolska967@gmail.com>
2026-03-08 12:02:32 +0500

    docs: rename to Pure Acorn

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-// (c) 2026 Lord of the Light Acorn: Veyyr3.
-// This file is part of Light Acorn and is distributed under the MIT License.
+// (c) 2026 Lord of the Pure Acorn: Veyyr3.
+// This file is part of Pure Acorn and is distributed under the MIT License.
 // See the LICENSES folder in the project root for the full license text.
 
 // src/main.rs
