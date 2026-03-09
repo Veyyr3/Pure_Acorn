0106b0f6050a6779590835da2dabdd6557ddfef1
Veyyr <dimapolska967@gmail.com>
2026-03-10 00:01:04 +0500

    chore: license comment

--- src/acorn_kernel/acorn_heart.rs
+++ src/acorn_kernel/acorn_heart.rs
@@ -1,6 +1,6 @@
-// This Source Code Form is subject to the terms of the Mozilla Public 
-// License, v. 2.0. If a copy of the MPL was not distributed with this 
-// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+// (c) 2026 Lord of the Pure Acorn: Veyyr3.
+// This file is part of Pure Acorn and is distributed under the AGPL-3.0 License.
+// See the LICENSE folder in the project root for the full license text.
 
 /* Copyright © 2026 Veyyr3
   Pure Acorn Framework: Kernel
