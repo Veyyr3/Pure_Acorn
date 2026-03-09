e90afe3b60b409e59ccb928b7b0de3e461cc2143
Veyyr <dimapolska967@gmail.com>
2026-03-09 23:57:42 +0500

    chore: add license

--- src/acorn_kernel/acorn_piston.rs
+++ src/acorn_kernel/acorn_piston.rs
@@ -1,11 +1,6 @@
-// This Source Code Form is subject to the terms of the Mozilla Public 
-// License, v. 2.0. If a copy of the MPL was not distributed with this 
-// file, You can obtain one at https://mozilla.org/MPL/2.0/.
-
-/* Copyright © 2026 Veyyr3
-  Pure Acorn Framework: Kernel
-  Lord of the Framework: Veyyr3
-*/
+// (c) 2026 Lord of the Pure Acorn: Veyyr3.
+// This file is part of Pure Acorn and is distributed under the AGPL-3.0 License.
+// See the LICENSE folder in the project root for the full license text.
 
 // src/acorn_kernel/acorn_piston.rs
 use crate::acorn_kernel::{
